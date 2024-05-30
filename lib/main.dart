9f7e5fabc4b42584b9082d846ff6c548b16a05bd
alexcao <alexcao194@gmail.com>
2024-05-30 16:37:08 +0700

    feat: remove unused package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:url_strategy/url_strategy.dart';
 
 import 'app.dart';
 import 'app/presentation/screen/settings/bloc/settings_bloc.dart';
@@ -10,11 +8,6 @@ import 'configs/di/di.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DI().init();
-
-  // if the app is running on the web, set the URL strategy (remove the # from the URL)
-  if (kIsWeb) {
-    setPathUrlStrategy();
-  }
 
   runApp(
     MultiBlocProvider(
