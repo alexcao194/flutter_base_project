d0e748fe62881ca9112e17cf627b82b48277c088
alexcao <alexcao194@gmail.com>
2024-06-05 11:53:28 +0700

    feat: remove DialogTools functions

--- lib/utils/tools/dialog_tools.dart
+++ lib/utils/tools/dialog_tools.dart
@@ -1,49 +1,5 @@
 import 'package:flutter/material.dart';
 
 class DialogTools {
-  static void showPopDialog({required BuildContext context, required Function onConfirm}) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("Xác nhận"),
-          content: const Text("Bạn có chắc chắn muốn thoát không?"),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("Hủy"),
-            ),
-            TextButton(
-              onPressed: () {
-                onConfirm();
-              },
-              child: const Text("Ok"),
-            ),
-          ],
-        );
-      },
-    );
-  }
 
-  static showQRDialog ({required BuildContext context, required String qrCode}) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("QR Code"),
-          content: Image.network(qrCode),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("Đóng"),
-            ),
-          ],
-        );
-      },
-    );
-  }
 }
